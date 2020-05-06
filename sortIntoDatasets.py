6927f8b86d5d55c1537874d0476621d44897d4f7
richmondvan <evan.ty.zheng@gmail.com>
2020-05-05 22:23:01 -0400

    Restarting the process!

---
+++ sortIntoDatasets.py
@@ -0,0 +1,46 @@
+import pathlib
+import shutil
+import os
+
+ROOT = "D:/Internet Downloads/ISIC-images/"
+BENIGN_PATH = ROOT + "benignPlaceholder/"
+MALIGNANT_PATH = ROOT + "malignantPlaceholder/"
+TRAINING_PATH = ROOT + "trainingUnmodififed/"
+VALIDATION_PATH = ROOT + "validation/"
+TEST_PATH = ROOT + "test/"
+
+ROOT_PATHS = [TRAINING_PATH, VALIDATION_PATH, TEST_PATH]
+CLASSES = ["benign/", "malignant/"]
+
+for root in ROOT_PATHS:
+    os.mkdir(root)
+    for classification in CLASSES:
+        os.mkdir(root+classification)
+
+
+benignPathList = pathlib.Path(BENIGN_PATH).glob("*.jpg")
+malignantPathList = pathlib.Path(MALIGNANT_PATH).glob("*.jpg")
+
+def distribute(pathList, classification):
+    global VALIDATION_PATH, TEST_PATH, TRAINING_PATH
+
+    counter = 0
+    for path in pathList:
+        name = path.name
+        copyPath = str(path)
+        counter += 1
+        key = counter % 10
+        if key == 1:
+            pastePath = VALIDATION_PATH + classification + name
+        elif key == 2:
+            pastePath = TEST_PATH + classification + name
+        else:
+            pastePath = TRAINING_PATH + classification + name
+        
+        shutil.move(copyPath, pastePath)
+
+distribute(benignPathList, "benign/")
+distribute(malignantPathList, "malignant/")
+
+os.rmdir(BENIGN_PATH)
+os.rmdir(MALIGNANT_PATH)
